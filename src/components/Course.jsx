45037168aebad5fe921253bac494693cb592e5a9
Santiago Florian <floranwsfrench@gmail.com>
2024-05-30 12:46:54 -0500

    Visualización de puntaje en estrellas

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,10 +1,9 @@
 import { useContext } from "react"
 import { Context } from "../auth/AuthContext"
+import { Rating } from "@mui/material"
+import { StarTwoTone } from "@mui/icons-material"
 
-
-
-
-const Course = ({name='Course', description='Course description', image='/src/assets/course-test.png', url='https://www.google.com', page="none", palette})=>{
+const Course = ({name='Course', description='Course description', image='/src/assets/course-test.png', url='https://www.google.com', page="none", palette, score})=>{
 
     const { user } = useContext(Context)
     const clicked = ()=>{
@@ -27,6 +26,7 @@ const Course = ({name='Course', description='Course description', image='/src/as
         <div style={{backgroundColor: palette.palette.primary.secondColor}} className="course">
             <img loading="lazy" className="course-image" src={image} alt="course-image" />
             <img className="site-icon" src={`/src/assets/${page}-logo.jpg`}></img>
+            <Rating sx={{position: 'absolute', top: '150px', right: '10px'}} className="score-course" icon={<StarTwoTone  fontSize="inherit" />} readOnly value={score}></Rating>
             <div style={{color: palette.palette.primary.contrastTextI}} className="course-info">
                 <div>
                 <h3>{name}</h3>
