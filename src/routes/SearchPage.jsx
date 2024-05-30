1730e4def5006c7730f9ac5ea1f9564d70d487c7
Santiago Florian <floranwsfrench@gmail.com>
2024-05-30 12:44:09 -0500

    Filtros y lectura de puntajes agregados

--- src/routes/SearchPage.jsx
+++ src/routes/SearchPage.jsx
@@ -1,8 +1,13 @@
 import {
   Button,
   Checkbox,
+  Divider,
   FormControl,
   FormControlLabel,
+  FormGroup,
+  FormLabel,
+  MenuItem,
+  Select,
   Tooltip,
   useTheme,
 } from '@mui/material'
@@ -27,45 +32,48 @@ export default function SearchPage() {
     querySearch.get('search-query') ? querySearch.get('search-query') : ''
   )
   const [courses, setCourses] = useState([])
-  const [error, setError] = useState({state: false, message: ''})
+  const [error, setError] = useState({ state: false, message: '' })
   const [loading, setLoading] = useState(false)
 
-  const pages = ['coursera', 'domestika', 'udacity', 'edX']
+  const pages = ['coursera', 'domestika', 'udacity']
 
-  const onClose=()=>{
-    setError({state: false, message: ''})
+  const onClose = () => {
+    setError({ state: false, message: '' })
   }
 
   const searchCourses = async () => {
     setLoading(true)
     let coursesObtained = []
-    await fetch(
-      'http://localhost:5000/search' + location.search,
-      {
-        credentials: 'include'
-      }
-    )
+    await fetch('http://localhost:5000/search' + location.search, {
+      credentials: 'include',
+    })
       .then((response) => response.json())
       .then((data) => {
         let dataCourses = []
         for (let pageOrigin in data['results']) {
           for (let course in data['results'][pageOrigin]) {
+            let numberScore = data['results'][pageOrigin][course]['Score']
+            if(pageOrigin==='coursera')numberScore=Number(numberScore.slice(0,4))
+            if(pageOrigin==='udacity')numberScore=Number(numberScore.slice(6,9))
+            if(pageOrigin==='domestika')numberScore=5
+
             dataCourses.push({
               name: data['results'][pageOrigin][course]['Title'],
               description: data['results'][pageOrigin][course]['Description'],
               image: data['results'][pageOrigin][course]['Image'],
               url: data['results'][pageOrigin][course]['CourseLink'],
+              score: numberScore,
               page: pageOrigin,
             })
           }
         }
 
         dataCourses.sort(() => 0.5 - Math.random())
-        
+
         coursesObtained = dataCourses
       })
-      .catch(e => {
-        setError({state: true, message: e.message})
+      .catch((e) => {
+        setError({ state: true, message: e.message })
       })
       .finally(() => {
         setLoading(false)
@@ -89,11 +97,15 @@ export default function SearchPage() {
 
   const handleSearch = (e) => {
     e.preventDefault()
-    let checkValues = ''
+    let values = ''
     pages.forEach((page) => {
-      checkValues += '&' + page + '=' + document.getElementById(page).checked
+      values += '&' + page + '=' + document.getElementById(page).checked
     })
-    navigate('/search?search-query=' + search + checkValues)
+    values += '&' + 'price' + '=' + document.getElementById('price').value
+    values += '&' + 'level' + '=' + document.getElementById('level').value
+    values += '&' + 'duration' + '=' + document.getElementById('duration').value
+    values += '&' + 'language' + '=' + document.getElementById('language').value
+    navigate('/search?search-query=' + search + values)
   }
 
   useEffect(() => {
@@ -129,22 +141,70 @@ export default function SearchPage() {
       <hr style={{ width: '100%' }}></hr>
 
       <div id='searchFilters'>
-        {pages.map((page, index) => {
-          return (
-            <FormControl key={index}>
-              <FormControlLabel
-                label={page}
-                control={
-                  <Checkbox
-                    name={page}
-                    id={page}
-                    defaultChecked={querySearch.get(page) === 'true'}
-                  ></Checkbox>
-                }
-              ></FormControlLabel>
-            </FormControl>
-          )
-        })}
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
+        <FormControl>
+          <FormLabel>Sitios</FormLabel>
+          <FormGroup row>
+            {pages.map((page, index) => {
+              return (
+                <FormControlLabel
+                  key={index}
+                  label={page}
+                  control={
+                    <Checkbox
+                      name={page}
+                      id={page}
+                      defaultChecked={querySearch.get(page) === 'true'}
+                    ></Checkbox>
+                  }
+                ></FormControlLabel>
+              )
+            })}
+          </FormGroup>
+        </FormControl>
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
+        <FormControl size='small' variant='standard' sx={{ width: '100px' }}>
+          <FormLabel>Precio</FormLabel>
+          <Select displayEmpty defaultValue={''} inputProps={{ id: 'price' }}>
+            <MenuItem value=''>Elegir..</MenuItem>
+            <MenuItem value={'free'}>Gratis</MenuItem>
+            <MenuItem value={'paid'}>Paga</MenuItem>
+          </Select>
+        </FormControl>
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
+        <FormControl size='small' variant='standard' sx={{ width: '110px' }}>
+          <FormLabel>Nivel</FormLabel>
+          <Select displayEmpty defaultValue={''} inputProps={{ id: 'level' }}>
+            <MenuItem value=''>Elegir..</MenuItem>
+            <MenuItem value={'begginer'}>Principante</MenuItem>
+            <MenuItem value={'intermediate'}>Intermedio</MenuItem>
+            <MenuItem value={'advanced'}>Avanzado</MenuItem>
+          </Select>
+        </FormControl>
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
+        <FormControl size='small' variant='standard' sx={{ width: '110px' }}>
+          <FormLabel>Duración</FormLabel>
+          <Select
+            displayEmpty
+            defaultValue={''}
+            inputProps={{ id: 'duration' }}
+          >
+            <MenuItem value=''>Elegir..</MenuItem>
+            <MenuItem value={'hours'}>Horas</MenuItem>
+            <MenuItem value={'days'}>Días</MenuItem>
+            <MenuItem value={'weeks'}>Semanas</MenuItem>
+          </Select>
+        </FormControl>
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
+        <FormControl size='small' variant='standard' sx={{ width: '100px' }}>
+          <FormLabel>Lenguaje</FormLabel>
+          <Select displayEmpty defaultValue={''} inputProps={{ id: 'language' }}>
+            <MenuItem value=''>Elegir..</MenuItem>
+            <MenuItem value={'spanish'}>Español</MenuItem>
+            <MenuItem value={'english'}>Inglés</MenuItem>
+          </Select>
+        </FormControl>
+        <Divider sx={{ borderWidth: '1px' }} variant='fullWidth' />
       </div>
       <hr style={{ width: '100%' }}></hr>
       <main>
@@ -155,7 +215,9 @@ export default function SearchPage() {
         </div>
       </main>
       {loading && <Loading type='search' />}
-      <DialogError open={error.state} closeError={onClose}>No se pudo procesar la solicitud</DialogError>
+      <DialogError open={error.state} closeError={onClose}>
+        No se pudo procesar la solicitud
+      </DialogError>
     </div>
   )
 }
